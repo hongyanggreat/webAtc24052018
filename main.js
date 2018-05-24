943ead70f1ad87292f05da5df6ee6029cf5f3bf9
duongnh <duongnh.nava@gmail.com>
2018-05-24 09:44:30 +0700

    web ART first

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log($);
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+    });
